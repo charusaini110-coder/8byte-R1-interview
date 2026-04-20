c64032c90aceaa135c161bd1fa1c5e5ab6cdf020
Charuuu <charuuu@Charuuus-MacBook-Neo.local>
2026-04-21 01:58:45 +0530

    updated files

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -6,8 +6,7 @@ module "vpc" {
   cidr = var.vpc_cidr
 
   azs             = ["${var.aws_region}a", "${var.aws_region}b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  public_subnets  = ["10.0.101.0/28"]
 
   enable_nat_gateway = var.enable_nat_gateway
   enable_dns_hostnames = true
